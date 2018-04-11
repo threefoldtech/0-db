16ded327fd59bd9e322c431758deff289e3df3f1
Maxime Daniel <root@maxux.net>
2018-04-11 03:57:05 +0200

    Improve test script, no code modification

--- tests/run.sh
+++ tests/run.sh
@@ -8,26 +8,33 @@ fi
 
 rm -rf /tmp/zdbtest
 
+# test arguments
 ./src/zdb --help || true
 ./src/zdb --blabla || true
 
+# test load with slashes
 ./src/zdb -v --dump --data /tmp/zdbtest --index /tmp/zdbtest
 ./src/zdb -v --dump --data /tmp/zdbtest/ --index /tmp/zdbtest/
-./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/
 
+# first real test suite
+./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/
 ./tests/zdbtests
 sleep 1
 
+# reopen existing data
 ./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/ --dump
 
+# simulate a segmentation fault
 ./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/
 pkill -SEGV zdb
 sleep 1
 
+# simulate a SIGINT (ctrl+c)
 ./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/
 pkill -INT zdb
 sleep 1
 
+# starting with authentification
 rm -rf /tmp/zdbtest
 
 ./src/zdb --background -v --socket /tmp/zdb.sock --data /tmp/zdbtest/ --index /tmp/zdbtest/ \
@@ -40,11 +47,25 @@ sleep 1
 
 rm -rf /tmp/zdbtest
 
-./src/zdb --background -v --data /tmp/zdbtest/ --index /tmp/zdbtest/ --admin root \
+# launch tcp testsuite
+# by the way, separate data and index directories
+./src/zdb --background -v --data /tmp/zdbtest-data/ --index /tmp/zdbtest-index/ --admin root \
   --logfile /tmp/zdb.logs \
   --listen 127.0.0.1 --port 9900 \
   --sync
 
 ./tests/zdbtests
 
-./src/zdb -v --data /tmp/zdbtest/ --index /tmp/zdbtest/ --dump --mode seq || true
+# open in sequential (will fails because created in another mode)
+./src/zdb -v --data /tmp/zdbtest-data/ --index /tmp/zdbtest-index/ --dump --mode seq || true
+
+# truncate index
+echo nopenopenope > /tmp/zdbtest-index/default/zdb-index-00000
+./src/zdb -v --data /tmp/zdbtest-data/ --index /tmp/zdbtest-index/ --dump || true
+
+# clean
+rm -rf /tmp/zdbtest-data
+rm -rf /tmp/zdbtest-index
+
+# create empty dataset in direct mode
+./src/zdb --data /tmp/zdbtest --index /tmp/zdbtest --dump --mode direct
