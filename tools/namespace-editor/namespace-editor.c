55119f684f976cf44ae0e05fa7c5b1a470b36438
Maxime Daniel <root@maxux.net>
2018-07-23 15:18:32 +0200

    Add working namespace-editor tool

--- tools/namespace-editor/namespace-editor.c
+++ tools/namespace-editor/namespace-editor.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -6,13 +7,23 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
-#include <x86intrin.h>
-#include <stdint.h>
-#include <inttypes.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <getopt.h>
 #include "zerodb.h"
 #include "index.h"
+#include "data.h"
+#include "namespace.h"
+
+static struct option long_options[] = {
+    {"index",     required_argument, 0, 'f'},
+    {"namespace", required_argument, 0, 'n'},
+    {"password",  required_argument, 0, 'p'},
+    {"maxsize",   required_argument, 0, 'm'},
+    {"public",    required_argument, 0, 'P'},
+    {"help",      no_argument,       0, 'h'},
+    {0, 0, 0, 0}
+};
 
 void *warnp(char *str) {
     fprintf(stderr, "[-] %s: %s\n", str, strerror(errno));
@@ -24,128 +35,165 @@ void diep(char *str) {
     exit(EXIT_FAILURE);
 }
 
-static char __hex[] = "0123456789abcdef";
-
-void hexdump(void *input, size_t length) {
-    unsigned char *buffer = (unsigned char *) input;
-    char *output = calloc((length * 2) + 1, 1);
-    char *writer = output;
-
-    for(unsigned int i = 0, j = 0; i < length; i++, j += 2) {
-        *writer++ = __hex[(buffer[i] & 0xF0) >> 4];
-        *writer++ = __hex[buffer[i] & 0x0F];
-    }
-
-    printf("0x%s", output);
-    free(output);
+void dies(char *str) {
+    fprintf(stderr, "[-] %s\n", str);
+    exit(EXIT_FAILURE);
 }
 
-static char *index_date(uint32_t epoch, char *target, size_t length) {
-    struct tm *timeval;
-    time_t unixtime;
+int directory_check(char *target) {
+    struct stat sb;
 
-    unixtime = epoch;
-
-    timeval = localtime(&unixtime);
-    strftime(target, length, "%F %T", timeval);
-
-    return target;
-}
-
-int index_dump(int fd) {
-    index_header_t header;
-    char entrydate[64];
-
-    // first step, let's validate the header
-    if(read(fd, &header, sizeof(index_header_t)) != (size_t) sizeof(index_header_t)) {
-        fprintf(stderr, "[-] cannot read index header\n");
+    if(stat(target, &sb) != 0)
         return 1;
-    }
 
-    if(memcmp(header.magic, "IDX0", 4) != 0) {
-        fprintf(stderr, "[-] index header magic mismatch\n");
-        return 1;
-    }
-
-    if(header.version != ZDB_IDXFILE_VERSION) {
-        fprintf(stderr, "[-] index version mismatch (%d <> %d)\n", header.version, ZDB_IDXFILE_VERSION);
-        return 1;
-    }
-
-    printf("[+] index header seems correct\n");
-
-    // now it's time to read each entries
-    // each time, one entry starts by the entry-header
-    // then entry payload.
-    // the entry headers starts with the amount of bytes
-    // of the key, which is needed to read the full header
-    uint8_t idlength;
-    index_item_t *entry = NULL;
-    size_t entrycount = 0;
-
-    while(read(fd, &idlength, sizeof(idlength)) == sizeof(idlength)) {
-        // we have the length of the key
-        ssize_t entrylength = sizeof(index_item_t) + idlength;
-        if(!(entry = realloc(entry, entrylength)))
-            diep("realloc");
-
-        // rollback the 1 byte read for the id length
-        off_t curoff = lseek(fd, -1, SEEK_CUR);
-
-        if(read(fd, entry, entrylength) != entrylength)
-            diep("index header read failed");
-
-        entrycount += 1;
-
-        index_date(entry->timestamp, entrydate, sizeof(entrydate));
-
-        printf("[+] index entry: %lu, offset: %lu\n", entrycount, curoff);
-        printf("[+]   id length  : %d\n", entry->idlength);
-        printf("[+]   data length: %" PRIu64 "\n", entry->length);
-        printf("[+]   data offset: %" PRIu64 "\n", entry->offset);
-        printf("[+]   data fileid: %u\n", entry->dataid);
-        printf("[+]   entry flags: 0x%X\n", entry->flags);
-        printf("[+]   entry date : %s\n", entrydate);
-        printf("[+]   previous   : %d\n", entry->previous);
-        printf("[+]   entry key  : ");
-        hexdump(entry->id, entry->idlength);
-        printf("\n");
-    }
-
-    printf("[+] ---------------------------\n");
-    printf("[+] all done, entry found: %lu\n", entrycount);
-
-    free(entry);
+    if(!S_ISDIR(sb.st_mode))
+        return 2;
 
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    char *filename = NULL;
+int namespace_edit(namespace_t *namespace) {
+    char *fullpath;
+    ns_header_t header;
     int fd;
-    struct stat sb;
 
-    if(argc < 2) {
-        fprintf(stderr, "Usage: %s index-filename\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if(asprintf(&fullpath, "%s/%s/zdb-namespace", namespace->indexpath, namespace->name) < 0)
+        diep("asprintf");
+
+    if((fd = open(fullpath, O_WRONLY | O_TRUNC | O_CREAT, 0600)) < 0)
+        diep(fullpath);
+
+    // fill-in the struct
+    header.namelength = strlen(namespace->name);
+    header.passlength = namespace->password ? strlen(namespace->password) : 0;
+    header.maxsize = namespace->maxsize;
+    header.flags = namespace->public ? NS_FLAGS_PUBLIC : 0;
+
+    if(write(fd, &header, sizeof(ns_header_t)) != sizeof(ns_header_t))
+        diep("write struct");
+
+    ssize_t length = (ssize_t) strlen(namespace->name);
+
+    if(write(fd, namespace->name, length) != length)
+        diep("write name");
+
+    if(namespace->password) {
+        length = strlen(namespace->password);
+
+        if(write(fd, namespace->password, length) != length)
+            diep("write password");
     }
 
-    filename = argv[1];
-    printf("[+] dumping index: %s\n", filename);
+    length = lseek(fd, SEEK_CUR, 0);
 
-    if(stat(filename, &sb) != 0)
-        diep(filename);
+    printf("[+] namespace descriptor written (%ld bytes)\n", length);
 
-    if(S_ISDIR(sb.st_mode)) {
-        fprintf(stderr, "[-] %s: target is a directory\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    if((fd = open(filename, O_RDONLY, 0600)) < 0)
-        diep(filename);
-
-    index_dump(fd);
     close(fd);
 
     return 0;
+}
+
+void usage() {
+    printf("Index rebuild tool arguments:\n\n");
+
+    printf("  --index      <dir>      index root directory path (required)\n");
+    printf("  --namespace  <name>     name of the namespace (required)\n");
+    printf("  --password   <pass>     password (default, empty)\n");
+    printf("  --maxsize    <size>     maximum size in bytes (default, no limit)\n");
+    printf("  --public     <bool>     does the namespace is public or not (yes or no, default yes)\n");
+    printf("  --help                  print this message\n");
+
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    int option_index = 0;
+    namespace_t namespace;
+    char *nspath;
+
+    // initializing everything to zero
+    memset(&namespace, 0x00, sizeof(namespace_t));
+
+    // set namespace to be public by default
+    namespace.public = 1;
+
+    while(1) {
+        // int i = getopt_long_only(argc, argv, "d:i:l:p:vxh", long_options, &option_index);
+        int i = getopt_long_only(argc, argv, "", long_options, &option_index);
+
+        if(i == -1)
+            break;
+
+        switch(i) {
+            case 'f':
+                namespace.indexpath = optarg;
+                break;
+
+            case 'n':
+                namespace.name = optarg;
+                break;
+
+            case 'p':
+                namespace.password = optarg;
+                break;
+
+            case 'm':
+                namespace.maxsize = atoll(optarg);
+                break;
+
+            case 'P':
+                if(strcmp(optarg, "yes") == 0) {
+                    namespace.public = 1;
+
+                } else if(strcmp(optarg, "no") == 0) {
+                    namespace.public = 0;
+
+                } else {
+                    fprintf(stderr, "[-] invalid value for --public (yes or no expected)\n");
+                    usage();
+                }
+
+                break;
+
+            case 'h':
+                usage();
+                break;
+
+            case '?':
+            default:
+               exit(EXIT_FAILURE);
+        }
+    }
+
+    if(!namespace.indexpath) {
+        fprintf(stderr, "[-] missing index root directory\n");
+        usage();
+    }
+
+    if(!namespace.name) {
+        fprintf(stderr, "[-] missing namespace name, you need to specify a name\n");
+        usage();
+    }
+
+    if(directory_check(namespace.indexpath)) {
+        if(mkdir(namespace.indexpath, 0775) < 0)
+            diep(namespace.indexpath);
+    }
+
+    if(asprintf(&nspath, "%s/%s", namespace.indexpath, namespace.name) < 0)
+        diep("asprintf");
+
+    if(directory_check(nspath)) {
+        if(mkdir(nspath, 0775) < 0)
+            diep(nspath);
+    }
+
+    printf("[+] zdb namespace editor\n");
+    printf("[+] directory: %s\n", namespace.indexpath);
+    printf("[+] namespace: %s\n", namespace.name);
+    printf("[+] public   : %s\n", namespace.public ? "yes" : "no");
+    printf("[+] password : %s\n", namespace.password ? "<protected>" : "<not set>");
+    printf("[+] max size : %.2f MB\n", MB(namespace.maxsize));
+
+    return namespace_edit(&namespace);
 }
