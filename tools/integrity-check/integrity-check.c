02ff8e2fd07da9edd6d5c5e0bb71fddc2416d8bf
Maxime Daniel <root@maxux.net>
2019-12-05 04:59:24 +0100

    Improve library usage for integrity-check code

--- tools/integrity-check/integrity-check.c
+++ tools/integrity-check/integrity-check.c
@@ -6,39 +6,12 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
-#include <x86intrin.h>
-#include <sys/stat.h>
+#include <libgen.h>
 #ifdef SHADUMP
 #include <openssl/sha.h>
 #endif
 #include "libzdb.h"
 #include "data.h"
-
-void *warnp(char *str) {
-    fprintf(stderr, "[-] %s: %s\n", str, strerror(errno));
-    return NULL;
-}
-
-void diep(char *str) {
-    warnp(str);
-    exit(EXIT_FAILURE);
-}
-
-static char __hex[] = "0123456789abcdef";
-
-void hexdump(void *input, size_t length) {
-    unsigned char *buffer = (unsigned char *) input;
-    char *output = calloc((length * 2) + 1, 1);
-    char *writer = output;
-
-    for(unsigned int i = 0, j = 0; i < length; i++, j += 2) {
-        *writer++ = __hex[(buffer[i] & 0xF0) >> 4];
-        *writer++ = __hex[buffer[i] & 0x0F];
-    }
-
-    printf("0x%s", output);
-    free(output);
-}
 
 #ifdef SHADUMP
 void sha256dump(char *input, size_t length) {
@@ -53,41 +26,24 @@ void sha256dump(char *input, size_t length) {
 }
 #endif
 
-uint32_t data_crc32(const uint8_t *bytes, ssize_t length) {
-    uint64_t *input = (uint64_t *) bytes;
-    uint32_t hash = 0;
-    ssize_t i = 0;
-
-    for(i = 0; i < length - 8; i += 8)
-        hash = _mm_crc32_u64(hash, *input++);
-
-    for(; i < length; i++)
-        hash = _mm_crc32_u8(hash, bytes[i]);
-
-    return hash;
-}
-
-int data_integrity(int fd) {
-    data_header_t header;
+int data_integrity(data_root_t *zdbdata) {
+    data_header_t *header;
+    char datestr[64];
     int errors = 0;
 
-    // first step, let's validate the header
-    if(read(fd, &header, sizeof(data_header_t)) != (size_t) sizeof(data_header_t)) {
-        fprintf(stderr, "[-] cannot read data header\n");
-        return 1;
-    }
+    // opening data file
+    zdbdata->datafd = zdb_data_open_readonly(zdbdata);
 
-    if(memcmp(header.magic, "DAT0", 4) != 0) {
-        fprintf(stderr, "[-] header magic mismatch\n");
+    // validating header
+    if(!(header = zdb_data_descriptor_load(zdbdata)))
         return 1;
-    }
 
-    if(header.version != ZDB_DATAFILE_VERSION) {
-        fprintf(stderr, "[-] version mismatch (%d <> %d)\n", header.version, ZDB_DATAFILE_VERSION);
+    if(!zdb_data_descriptor_validate(header, zdbdata))
         return 1;
-    }
 
-    printf("[+] data header seems correct\n");
+    printf("[+] integrity-check: header seems correct\n");
+    printf("[+] integrity-check: created at: %s\n", zdb_header_date(header->created, datestr, sizeof(datestr)));
+    printf("[+] integrity-check: last open: %s\n", zdb_header_date(header->opened, datestr, sizeof(datestr)));
 
     // now it's time to read each entries
     // each time, one entry starts by the entry-header
@@ -99,17 +55,17 @@ int data_integrity(int fd) {
     size_t entrycount = 0;
     char *buffer = NULL;
 
-    while(read(fd, &idlength, sizeof(idlength)) == sizeof(idlength)) {
+    while(read(zdbdata->datafd, &idlength, sizeof(idlength)) == sizeof(idlength)) {
         // we have the length of the key
         ssize_t entrylength = sizeof(data_entry_header_t) + idlength;
         if(!(entry = realloc(entry, entrylength)))
-            diep("realloc");
+            zdb_diep("realloc");
 
         // rollback the 1 byte read for the id length
-        off_t current = lseek(fd, -1, SEEK_CUR);
+        off_t current = lseek(zdbdata->datafd, -1, SEEK_CUR);
 
-        if(read(fd, entry, entrylength) != entrylength)
-            diep("data header read failed");
+        if(read(zdbdata->datafd, entry, entrylength) != entrylength)
+            zdb_diep("data header read failed");
 
         entrycount += 1;
 
@@ -118,19 +74,19 @@ int data_integrity(int fd) {
         printf("[+]   previous offset: %u\n", entry->previous);
         printf("[+]   expected crc: %08x\n", entry->integrity);
         printf("[+]   entry key: ");
-        hexdump(entry->id, entry->idlength);
+        zdb_tools_hexdump(entry->id, entry->idlength);
         printf("\n");
 
         if(entry->datalength == 0)
             continue;
 
         if(!(buffer = realloc(buffer, entry->datalength)))
-            diep("realloc");
+            zdb_diep("realloc");
 
-        if(read(fd, buffer, entry->datalength) != entry->datalength)
-            diep("payload entry read failed");
+        if(read(zdbdata->datafd, buffer, entry->datalength) != entry->datalength)
+            zdb_diep("payload entry read failed");
 
-        uint32_t crc = data_crc32((uint8_t *) buffer, entry->datalength);
+        uint32_t crc = zdb_checksum_crc32((uint8_t *) buffer, entry->datalength);
 
         if(crc != entry->integrity) {
             fprintf(stderr, "[-] integrity check failed: %08x <> %08x\n", crc, entry->integrity);
@@ -153,33 +109,58 @@ int data_integrity(int fd) {
 }
 
 int main(int argc, char *argv[]) {
-    char *filename = NULL;
-    int val = 0, fd;
-    struct stat sb;
-
     if(argc < 2) {
         fprintf(stderr, "Usage: %s data-filename\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    filename = argv[1];
+    char *filename = argv[1];
     printf("[+] checking data integrity of: %s\n", filename);
 
-    if(stat(filename, &sb) != 0)
-        diep(filename);
-
-    if(S_ISDIR(sb.st_mode)) {
-        fprintf(stderr, "[-] %s: target is a directory\n", filename);
+    if(zdb_file_exists(filename) != ZDB_FILE_EXISTS) {
+        fprintf(stderr, "[-] %s: invalid target file\n", filename);
         exit(EXIT_FAILURE);
     }
 
-    if((fd = open(filename, O_RDONLY, 0600)) < 0)
-        diep(filename);
+    // splitting directory and file from argument
+    char *filedir = dirname(strdup(filename));
+    char *datafile = basename(filename);
 
-    if((val = data_integrity(fd)))
+    if(strncmp(datafile, "zdb-data-", 9)) {
+        fprintf(stderr, "[-] integrity-check: data filename seems wrong\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint16_t dataid = atoi(datafile + 9);
+
+    printf("[+] datafile path: %s\n", filedir);
+    printf("[+] datafile name: %s\n", datafile);
+    printf("[+] datafile id  : %d\n", dataid);
+
+    zdb_settings_t *zdb_settings = zdb_initialize();
+    zdb_id_set("integrity-checker");
+
+    // WARNING: we *don't* open the database, this would populate
+    //          memory, reading and loading everything... we just want
+    //          to initialize structs but not loads anything
+    //
+    //          we will shortcut the database loading and directly
+    //          call the low-level data lazy loader (lazy loader won't
+    //          load anything except structs)
+    //
+    // zdb_open(zdb_settings);
+    data_root_t *zdbdata;
+
+    if(!(zdbdata = zdb_data_init_lazy(zdb_settings, filedir, dataid))) {
+        fprintf(stderr, "[-] integrity-check: cannot load data\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int val = 0;
+
+    if((val = data_integrity(zdbdata)))
         fprintf(stderr, "[-] data file inconsistency: %d error(s)\n", val);
-
-    close(fd);
 
     return val;
 }
+
