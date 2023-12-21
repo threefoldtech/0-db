fe32a2744c444e011afe9e508a2fc19e47ebad43
Maxime Daniel <root@maxux.net>
2023-12-21 21:44:38 +0100

    data: ensure file pre-allocation before writing
    Data files are intended to be used on HDD when used on storage purpose,
    pre-allocating data space (without really write actual data) ensure
    that enough space is available and reduce fragmentation.
    
    If this call fails, allocation is just ignored and old behavior is used.

--- libzdb/data.c
+++ libzdb/data.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -250,6 +252,11 @@ void data_initialize(char *filename, data_root_t *root) {
 
         zdb_diep(filename);
     }
+
+    // pre-allocate data size to avoid fragmentation
+    zdb_settings_t *settings = zdb_settings_get();
+    if(fallocate(fd, FALLOC_FL_KEEP_SIZE, 0, settings->datasize) < 0)
+        zdb_warnp(filename);
 
     // writing initial header
     data_header_t header;
